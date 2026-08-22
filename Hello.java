4d38d79c88cc0995f99a53b8d4e9297f989ad517
hasinibanda-24 <hasinibanda24@gmail.com>
2026-08-22 11:17:56 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,10 +1,10 @@
 class Hello {
 	public static void main(string[] args) {
-		string name = System.getenv("USERNAME_VAR"):
+		string name = System.getenv("USERNAME_VAR");
 		if (name != null) {
-			system.out.println("Hello",+ " "+name):
+			system.out.println("Hello",+ " "+name);
 		}else {
-			system.out.println("Environment variable not set!"):
+			system.out.println("Environment variable not set!");
 		}
 	}
 }
