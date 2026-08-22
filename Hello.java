073becd1572a3ce8859e5fe1a6c353b1271192ce
hasini <hasinibanda24@gmail.com>
2026-08-22 10:14:20 +0530

    Hello.java file added successfully

---
+++ Hello.java
@@ -0,0 +1,12 @@
+cat Hello,java
+public class Hello {
+	pudlic static void main(string[] args) {
+		string name = system,getenv("USERNAME_VAR"):
+		if (name 1 = null) {
+			system.out.println("Hello. "+name):
+		}else {
+			system.out.println("Environment variable not set!"):
+		}
+	}
+}
+
