1c0574686f38e1eefe58d6c413e7bb21901a2028
hasinibanda-24 <hasinibanda24@gmail.com>
2026-08-22 11:16:56 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,8 @@
-cat Hello,java
-public class Hello {
-	pudlic static void main(string[] args) {
-		string name = system,getenv("USERNAME_VAR"):
-		if (name 1 = null) {
-			system.out.println("Hello. "+name):
+class Hello {
+	public static void main(string[] args) {
+		string name = System.getenv("USERNAME_VAR"):
+		if (name != null) {
+			system.out.println("Hello",+ " "+name):
 		}else {
 			system.out.println("Environment variable not set!"):
 		}
